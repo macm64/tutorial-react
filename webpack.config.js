448055b433915748f24f199a2f8684b521428a6a
root <root@MiguelC>
2018-05-15 08:41:13 -0300

    MASTER

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+
+module.exports: {
+  entry: {
+    app: path.join(__dirname, '/src/index.js')
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css']
+  },
+  devServer: {
+    host: 'localhost',
+    port: 3000,
+    inline: true
+  },
+  module: {
+    loaders: [{
+      test: /(\.js|.jsx)$/,
+      loader: 'babel-loader',
+      excludes: '/node-modules/',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    } {
+      test: /(\.css)$/,
+      loader: 'style-loader'
+    } {
+      test: /(\.css)$/,
+      loader: 'css-loader'
+    }]
+  }
+}
